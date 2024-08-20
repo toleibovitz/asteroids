c37642954d3b71410fa522e718e56f191931c3f1
toleibovitz <toleibovitz@gmail.com>
2024-08-20 10:16:50 -0500

    Added logic for updating and drawing objects in groups

--- main.py
+++ main.py
@@ -2,21 +2,39 @@ import pygame
 from constants import *
 from player import *
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable )
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, PLAYER_RADIUS)
     dt = 0
+    updatable.add(player)
+    drawable.add(player)
+    print(updatable)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for obj in updatable:
+            obj.update(dt)
+        
         screen.fill('black')
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+        
+        
         dt = clock.tick(60) / 1000
-    
 
 
 if __name__ == "__main__":
