7bcc3c25235bafe3864f351d58b2761adf15952f
toleibovitz <toleibovitz@gmail.com>
2024-08-20 09:21:56 -0500

    Added logic for drawing player object

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
-    print("Starting asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, PLAYER_RADIUS)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+    
 
 
 if __name__ == "__main__":
