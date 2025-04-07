8c452089f384ad6f2a18806fec84e2ae4af5143e
toleibovitz <toleibovitz@gmail.com>
2025-04-06 22:26:26 -0500

    updated game loop and added CirleShape class

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
         
 
 
