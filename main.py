4b6e3f8696a51b595551b714f6bf043ad407d7a4
toleibovitz <toleibovitz@gmail.com>
2025-04-19 16:40:33 -0500

    added asteriods logic

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+   
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        updateable.update(dt)    
         screen.fill("black")
+        
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
         
 
 
